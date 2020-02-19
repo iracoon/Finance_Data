7b83ed360974c932636b09617df394e49dac80e2
Rachana <43420002+iracoon@users.noreply.github.com>
2020-02-19 10:50:56 -0600

    split up templates, added industry null and industry filters

--- analysis/views.py
+++ analysis/views.py
@@ -18,8 +18,30 @@ def search(request):
 	nulls_param_query = request.GET.get('nulls_param')
 	keywords_param_query = request.GET.get('keywords_param')
 
+	# keywords search for all columns in table
 	if keywords_param_query != '' and keywords_param_query is not None:
-		qs = qs.filter(Orgname__icontains = keywords_param_query)
+		qs = qs.filter(Q(ID__icontains = keywords_param_query)
+			| Q(Chamber__icontains = keywords_param_query)
+			| Q(CID__icontains = keywords_param_query)
+			| Q(CalendarYear__icontains = keywords_param_query)
+			| Q(ReportType__icontains = keywords_param_query)
+			| Q(CompSource__icontains = keywords_param_query)
+			| Q(Orgname__icontains = keywords_param_query)
+			| Q(Ultorg__icontains = keywords_param_query)
+			| Q(Realcode__icontains = keywords_param_query)
+			| Q(Source__icontains = keywords_param_query)
+			| Q(CompSourceLocation__icontains = keywords_param_query)
+			| Q(CompDuties__icontains = keywords_param_query)
+			| Q(dupe__icontains = keywords_param_query))
+
+	# specify by industry, or select all industries
+	if industry_param_query != '' and industry_param_query is not None:
+		qs = qs.filter(Realcode=industry_param_query)
+
+	# include nulls in industry column
+	if nulls_param_query == 'on':
+		print("IM CALLED")
+		qs = qs.exclude(Realcode__isnull=True).exclude(Realcode__exact='')
 
 	context = {
 		'queryset': qs
