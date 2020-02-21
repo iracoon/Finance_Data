b97e2a76f172bcc14dda692d159b22f6991df1c5
Rachana <podarallarachana@ufl.edu>
2020-02-21 15:23:42 -0600

    added more styles to profiles/details page

--- analysis/urls.py
+++ analysis/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from .views import ResearchListView, ResearchDetailView, SearchDetailView, SearchListView
 from . import views
 
 urlpatterns = [
 	path('', views.home, name='analysis-home'),
 	path('about/', views.about, name='analysis-about'),
-	path('search/', views.search, name='analysis-search')
+	path('research/', ResearchListView.as_view(), name='analysis-research'),
+	path('research/<int:pk>/', ResearchDetailView.as_view(), name='analysis-research-detail'),
+	path('search/',  SearchListView.as_view(), name='analysis-search'),
+	path('search/<pk>/', SearchDetailView.as_view(), name='analysis-search-detail')
 ]
--- analysis/views.py
+++ analysis/views.py
@@ -1,53 +1,71 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q
+from django.views.generic import ListView, DetailView
 from .models import Compensation
 from .models import Post
 
 def home(request):
 	return render(request, 'analysis/home.html')
 
-
 def about(request):
 	return render(request, 'analysis/about.html')
 
+# def research(request):
+# 	context = {
+# 		'posts' : Post.objects.all()
+# 	}
+# 	return render(request, 'analysis/research.html', context)
 
-def search(request):
-	qs = Compensation.objects.all()
+class ResearchListView(ListView):
+	model = Post
+	template_name = 'analysis/research.html'
+	context_object_name = 'posts'
+	ordering = ['-date_posted']
 
-	industry_param_query = request.GET.get('industry_param')
-	nulls_param_query = request.GET.get('nulls_param')
-	keywords_param_query = request.GET.get('keywords_param')
+class ResearchDetailView(DetailView):
+	model = Post
+	template_name = 'analysis/research_detail.html'
 
-	# keywords search for all columns in table
-	if keywords_param_query != '' and keywords_param_query is not None:
-		qs = qs.filter(Q(ID__icontains = keywords_param_query)
-			| Q(Chamber__icontains = keywords_param_query)
-			| Q(CID__icontains = keywords_param_query)
-			| Q(CalendarYear__icontains = keywords_param_query)
-			| Q(ReportType__icontains = keywords_param_query)
-			| Q(CompSource__icontains = keywords_param_query)
-			| Q(Orgname__icontains = keywords_param_query)
-			| Q(Ultorg__icontains = keywords_param_query)
-			| Q(Realcode__icontains = keywords_param_query)
-			| Q(Source__icontains = keywords_param_query)
-			| Q(CompSourceLocation__icontains = keywords_param_query)
-			| Q(CompDuties__icontains = keywords_param_query)
-			| Q(dupe__icontains = keywords_param_query))
+class SearchDetailView(DetailView):
+	model = Compensation
+	template_name = 'analysis/search_detail.html'
 
-	# specify by industry, or select all industries
-	if industry_param_query != '' and industry_param_query is not None:
-		qs = qs.filter(Realcode=industry_param_query)
+class SearchListView(ListView):
+	model = Compensation
+	context_object_name = 'compensations'
+	template_name = 'analysis/search.html'
 
-	# include nulls in industry column
-	if nulls_param_query == 'on':
-		print("IM CALLED")
-		qs = qs.exclude(Realcode__isnull=True).exclude(Realcode__exact='')
+	def get_queryset(self):
+		qs = Compensation.objects.all()
 
-	context = {
-		'queryset': qs,
-		'posts' : Post.objects.all()
-	}
+		industry_param_query = self.request.GET.get('industry_param')
+		nulls_param_query = self.request.GET.get('nulls_param')
+		keywords_param_query = self.request.GET.get('keywords_param')
 
-	return render(request, 'analysis/search.html', context)
+		# keywords search for all columns in table
+		if keywords_param_query != '' and keywords_param_query is not None:
+			qs = qs.filter(Q(ID__icontains = keywords_param_query)
+				| Q(Chamber__icontains = keywords_param_query)
+				| Q(CID__icontains = keywords_param_query)
+				| Q(CalendarYear__icontains = keywords_param_query)
+				| Q(ReportType__icontains = keywords_param_query)
+				| Q(CompSource__icontains = keywords_param_query)
+				| Q(Orgname__icontains = keywords_param_query)
+				| Q(Ultorg__icontains = keywords_param_query)
+				| Q(Realcode__icontains = keywords_param_query)
+				| Q(Source__icontains = keywords_param_query)
+				| Q(CompSourceLocation__icontains = keywords_param_query)
+				| Q(CompDuties__icontains = keywords_param_query)
+				| Q(dupe__icontains = keywords_param_query))
+
+		# specify by industry, or select all industries
+		if industry_param_query != '' and industry_param_query is not None:
+			qs = qs.filter(Realcode=industry_param_query)
+
+		# include nulls in industry column
+		if nulls_param_query == 'on':
+			qs = qs.exclude(Realcode__isnull=True).exclude(Realcode__exact='')
+
+		return qs;
 
