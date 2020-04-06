b6b5d674391922dc874965a6dc6a8c15a49fcb43
iracoon <podarallarachana@ufl.edu>
2020-04-06 11:55:02 -0500

    Agreement upload script

--- analysis/management/commands/create_data.py
+++ analysis/management/commands/create_data.py
@@ -2,10 +2,18 @@ import pandas as pd
 
 from django.core.management.base import BaseCommand
 from analysis.models import Compensation
+from analysis.models import Agreement
+
+# python manage.py makemigrations (1)
+# python manage.py migrate (2)
+
+#(3)
+#python manage.py create_data data/PFDcomp.txt analysis_compensation
+#python manage.py create_data data/PFDagree.txt analysis_agreement
 
 class Command(BaseCommand):
 
-    def add_arguments(self, parser): #python manage.py create_data PFDcomp.txt analysis_compensation
+    def add_arguments(self, parser): 
         parser.add_argument(
             'file_name', type=str, help='The txt file that contains the table records.')
         parser.add_argument(
@@ -18,34 +26,92 @@ class Command(BaseCommand):
         if(model_name == "analysis_compensation"): #delete all records in table
             Compensation.objects.all().delete()
 
+        if(model_name == "analysis_agreement"): #delete all records in table
+            Agreement.objects.all().delete()
+
         num_rows = 0
             
         with open(f'{file_name}') as file: #read through each line in txt file, create a record, save 
-            for row in file:
-                cols = row.split('|,')
-                tmp_arr = []
-
-                for col in cols:
-                    cleaned_col = col.replace('|', '').strip()
-                    tmp_arr.append(cleaned_col)
-
                 if(model_name == "analysis_compensation"):
-                    comp_record = Compensation(
-                        ID = tmp_arr[0],
-                        Chamber = tmp_arr[1],
-                        CID = tmp_arr[2],
-                        CalendarYear = tmp_arr[3],
-                        ReportType = tmp_arr[4],
-                        CompSource = tmp_arr[5],
-                        Orgname = tmp_arr[6],
-                        Ultorg = tmp_arr[7],
-                        Realcode = tmp_arr[8],
-                        Source = tmp_arr[9],
-                        CompSourceLocation = tmp_arr[10],
-                        CompDuties = tmp_arr[11],
-                        dupe = tmp_arr[12])
-                    print(comp_record)
-                    comp_record.save();
-                    num_rows += 1;
+                    for row in file:
+                        cols = row.split('|,')
+                        tmp_arr = []
+                        for col in cols:
+                            cleaned_col = col.replace('|', '').strip()
+                            tmp_arr.append(cleaned_col)
+                        print(tmp_arr)
+                        comp_record = Compensation(
+                            ID = tmp_arr[0],
+                            Chamber = tmp_arr[1],
+                            CID = tmp_arr[2],
+                            CalendarYear = tmp_arr[3],
+                            ReportType = tmp_arr[4],
+                            CompSource = tmp_arr[5],
+                            Orgname = tmp_arr[6],
+                            Ultorg = tmp_arr[7],
+                            Realcode = tmp_arr[8],
+                            Source = tmp_arr[9],
+                            CompSourceLocation = tmp_arr[10],
+                            CompDuties = tmp_arr[11],
+                            dupe = tmp_arr[12])
+                        comp_record.save();
+                        num_rows += 1;
+
+                if(model_name == "analysis_agreement"):
+                    prev_ind = 0
+                    all_rows = []
+                    for row in file:
+                        if row[0] != "|":
+                            all_rows[prev_ind - 1] = all_rows[prev_ind - 1][:-1] + row
+                        else:
+                            all_rows.append(row)
+                            prev_ind += 1
+
+                    for row in all_rows:
+                        print(row)
+                        tmp_arr = []
+                        isStringWord = False
+                        word = ""
+                        for char in row:
+                            if char == row[-1]:
+                                tmp_arr.append(word)
+                            if char == "," and isStringWord == False:
+                                tmp_arr.append(word)
+                                word = ""
+                            elif char == "," and isStringWord == True:
+                                word += char
+                            elif char == "|" and isStringWord == False:
+                                isStringWord = True
+                            elif char == "|" and isStringWord == True:
+                                isStringWord = False
+                            else:
+                                word += char
+                        print(tmp_arr)
+                        print(tmp_arr[21])
+                        comp_record = Agreement(
+                            ID = tmp_arr[0],
+                            Chamber = tmp_arr[1],
+                            CID = tmp_arr[2],
+                            CalendarYear = tmp_arr[3],
+                            ReportType = tmp_arr[4],
+                            AgreementDate1 = tmp_arr[5],
+                            AgreementDate1Text = tmp_arr[6],
+                            AgreementDate2 = tmp_arr[7],
+                            AgreementDate2Text = tmp_arr[8],
+                            AgreementParty1 = tmp_arr[9],
+                            Orgname = tmp_arr[10],
+                            Ultorg = tmp_arr[11],
+                            Realcode = tmp_arr[12],
+                            Source = tmp_arr[13],
+                            AgreementParty1Loc = tmp_arr[14],
+                            AgreementParty2 = tmp_arr[15],
+                            Orgname2 = tmp_arr[16],
+                            Ultorg2 = tmp_arr[17],
+                            Realcode2 = tmp_arr[18],
+                            Source2 = tmp_arr[19],
+                            AgreementTerms = tmp_arr[20],
+                            dupe = tmp_arr[21])
+                        comp_record.save();
+                        num_rows += 1;
 
         self.stdout.write(self.style.SUCCESS(str(num_rows) + ' records uploaded successfully!'))
--- analysis/models.py
+++ analysis/models.py
@@ -3,9 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-
-# python manage.py makemigrations
-# python manage.py migrate
+# drop tables
+# delete old migration files for table if they exist
+# DELETE FROM finance_data.django_migrations WHERE app='analysis';
+# python ./manage.py makemigrations analysis (1)
+# python ./manage.py migrate analysis (2)
 
 class Compensation(models.Model):
 	ID = models.CharField(max_length=15, primary_key=True)
@@ -54,6 +56,64 @@ class Compensation(models.Model):
 # [CompDuties] [varchar](100) NULL,
 # [dupe] [char](1) NULL
 # ) ON [PRIMARY]
+
+########################################################
+
+class Agreement(models.Model):
+	ID = models.CharField(max_length=15, primary_key=True)
+	Chamber = models.CharField(max_length=1, default=None)
+	CID = models.CharField(max_length=9, default=None)
+	CalendarYear = models.CharField(max_length=2, default=None)
+	ReportType = models.CharField(max_length=1, default=None)
+	AgreementDate1 = models.CharField(max_length=100,default=None)
+	AgreementDate1Text = models.CharField(max_length=100, default=None)
+	AgreementDate2 = models.CharField(max_length=100, default=None)
+	AgreementDate2Text = models.CharField(max_length=100, default=None)
+	AgreementParty1 = models.CharField(max_length=100, default=None)
+	Orgname = models.CharField(max_length=100, default=None)
+	Ultorg = models.CharField(max_length=100, default=None)
+	Realcode = models.CharField(max_length=100, default=None)
+	Source = models.CharField(max_length=100, default=None)
+	AgreementParty1Loc = models.CharField(max_length=100, default=None)
+	AgreementParty2 = models.CharField(max_length=100, default=None)
+	Orgname2 = models.CharField(max_length=100, default=None)
+	Ultorg2 = models.CharField(max_length=100, default=None)
+	Realcode2 = models.CharField(max_length=100, default=None)
+	Source2 = models.CharField(max_length=100, default=None)
+	AgreementTerms = models.TextField(default=None)
+	dupe = models.CharField(max_length=1,default=None)
+
+	def __str__(self):
+		obj_str = 'ID: ' + self.ID 
+		return obj_str
+
+# CREATE TABLE [dbo].[Agreement](
+# [ID] [varchar](15) NOT NULL,
+# [Chamber] [char](1) NULL,
+# [CID] [char](9) NULL,
+# [CalendarYear] [char](2) NOT NULL,
+# [ReportType] [char](1) NULL,
+# [AgreementDate1] [smalldatetime] NULL,
+# [AgreementDate1Text] [char](50) NULL,
+# [AgreementDate2] [smalldatetime] NULL,
+# [AgreementDate2Text] [char](50) NULL,
+# [AgreementParty1] [varchar](100) NULL,
+# [Orgname] [varchar](40) NULL,
+# [Ultorg] [varchar](40) NULL,
+# [Realcode] [char](5) NULL,
+# [Source] [char](5) NULL,
+# [AgreementParty1Loc] [varchar](50) NULL,
+# [AgreementParty2] [varchar](100) NULL,
+# [Orgname2] [varchar](40) NULL,
+# [Ultorg2] [varchar](40) NULL,
+# [Realcode2] [char](5) NULL,
+# [Source2] [char](5) NULL,
+# [AgreementTerms] [varchar](1500) NULL,
+# [Dupe] [char](1) NULL
+# ) ON [PRIMARY]
+
+
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
